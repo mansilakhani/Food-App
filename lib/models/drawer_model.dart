8b3593c247a6ed398091000ca8a6a853cb8700d5
mansilakhani <111499824+mansilakhani@users.noreply.github.com>
2022-12-02 04:59:51 +0530

    Delete drawer_model.dart

--- lib/models/drawer_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DrawerModel {
-  final IconData? icon;
-  final String? name;
-  String? route;
-
-  DrawerModel({required this.icon, required this.name, this.route});
-}
